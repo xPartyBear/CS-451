e8c5c11653774c8c7136c213b44741ca6cf74e83
xPartyBear <michaeldekoski@gmail.com>
2022-11-04 22:01:07 -0400

    rrScheduler.c is Complete prime.c is Complete README.txt is Complete

--- dekoskitarnowski2/prime.c
+++ dekoskitarnowski2/prime.c
@@ -1,31 +1,56 @@
+/*
+Author: Michael Dekoski & Evan Tarnowski
+Assignment Number: 2
+Date of Submission: 11-04-2022
+Name of this file: prime.c
+Short description of contents:
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <time.h>
 #include <stdbool.h>
 #include <math.h>
-#include <time.h>
 
-void handler(int signum);
-void resume();
-void suspend();
-void terminate();
+const int suspend_signal = 10;
+const int continue_signal = 11;
+const int terminate_signal = 12;
 
 long unsigned int rand_num;
 long unsigned int prime;
-int boolean = 0;
+int process;
+int flag = 0;
 
+/*
+Function Name: rand_num_gen
+Input to the method: None
+Output(Return value): a random number as rand_num
+Brief description of the task:
+generate a random number for each child to use
+*/
 long unsigned int rand_num_gen() {
     time_t t;
-    long unsigned int rand_max = 9999999999;
-    long unsigned int rand_min = 1000000000;
+    long unsigned int rand_max = 9999999999;//max the random number can be
+    long unsigned int rand_min = 1000000000;//min the random number can be
 
-    srand((unsigned) time(&t));
-    rand_num = rand_min + rand() % (rand_max + 1 - rand_min);
-    printf("%lu\n", rand_num);
+    srand((unsigned) time(&t));//set the seed of rand with the system time
+    rand_num = rand_min + rand() % (rand_max + 1 - rand_min);//make random number
+//    printf("%lu\n", rand_num);
     return rand_num;
 }
 
+/*
+Function Name: prime_check
+Input to the method: prime to check as num
+Output(Return value): num(the next prime number)
+Brief description of the task:
+check for primes starting at num
+*/
 long unsigned int prime_check(long unsigned int num) {
     bool check = false;
+    //while the number is not prime, increment num until the next prime is found
     while(check == false) {
         num++;
         long i;
@@ -33,7 +58,7 @@ long unsigned int prime_check(long unsigned int num) {
         check = true;
         m = num / 2;
         for(i = 2; i <= m; i++) {
-            if (num % i == 0) {
+            if (num % i == 0) {//if num is divisible by i, it is not prime
                 //printf("Entered number is not prime");
                 check = false;
                 break;
@@ -45,56 +70,118 @@ long unsigned int prime_check(long unsigned int num) {
     return num;
 }
 
-int main() {
-    //Start handlers
-    signal(sigStart, handler);
-    signal(sigSus, handler);
-    signal(sigExit, handler);
-
-    //long unsigned int rand_num = rand_num_gen();
-    rand_num = rand_num_gen();
-    //prime = prime_check(prime);
-
-    return 0;
+/*
+Function Name: start
+Input to the method: None
+Output(Return value): None
+Brief description of the task:
+print the start message and check primes
+*/
+void start(){
+    //print start message
+    printf("Process %d: My PID is %d: I just got started.\n"
+           "I am starting with the number %lu to find the next prime number.\n\n", process, getpid(), rand_num);
+    fflush(stdout);
+    prime = prime_check(rand_num);//check if the random number is prime
+    while(flag == 0){
+        prime = prime_check(prime);//check all primes after that
+    }
 }
 
-void handler(int signum){
-// Write handler code here.
-
-    if (signum == sigStart) {
-        boolean = 1;
-        resume();
+/*
+Function Name: resume
+Input to the method: None
+Output(Return value): None
+Brief description of the task:
+print the resumed message and check primes
+*/
+void resume(){
+    //print resumed message
+    printf("Process %d: My PID is %d: I just got resumed.\n\n", process, getpid());
+    fflush(stdout);
+    while(flag == 0) {//while flag is 0(process is resumed)
+        prime = prime_check(prime);//check primes
     }
-    if (signum == sigSus) {
-        boolean = 0;
-        //resume();
-//            sleep(.4);
+}
+
+/*
+Function Name: suspend
+Input to the method: None
+Output(Return value): None
+Brief description of the task:
+print the suspension message
+*/
+void suspend(){
+    //print suspension message
+    printf("Process %d: my PID is %d: I am about to be suspended...\n"
+           "Highest prime number I found is %lu.\n\n",process, getpid(), prime);
+    fflush(stdout);
+}
+
+/*
+Function Name: terminate
+Input to the method: None
+Output(Return value): None
+Brief description of the task:
+print the completion message
+*/
+void terminate(){
+    //print completion message
+    printf("Process %d: my PID is %d: I completed my task and I am exiting.\n"
+           "Highest prime number I found is %lu.\n\n",process, getpid(), prime);
+    fflush(stdout);
+}
+
+/*
+Function Name: child_handler
+Input to the method: signal as signum
+Output(Return value): None
+Brief description of the task:
+The handler for receiving signals from the parent
+*/
+void child_handler(int signum){
+    if(signum == suspend_signal){//if suspend signal
+        flag = 1;
         suspend();
     }
-    if (signum == sigExit) {
-        //sleep(.3);
+    else if(signum == continue_signal){//if continue signal
+        flag = 0;
+        resume();
+    }
+    else if(signum == terminate_signal) {//if terminate signal
+        flag = -1;
         terminate();
     }
 }
 
-void resume(){
-    //start
-    if (boolean == 1) {
-        printf("Process %d: My PID is %d: I just got started. \n"
-               "I am starting with the number %llu to find the next prime number.\n", process, getpid(), test);
-    }
+/*
+Function Name: main
+Input to the method: Number of arguments(argc) and arguments(argv)
+Output(Return value): 0
+Brief description of the task:
+init the signals, stay until terminated
+*/
+int main(int argc, char* argv[]){
 
-    while(true){
-        prime = prime_check(prime);
-    }
-}
-void suspend(){
-    printf("Process %d: my PID is %d: I am about to be suspended... Highest\n"
-           "prime number I found is %llu.\n",process, getpid(), prime);
+    //set process to the process from input.txt
+    process = atoi(argv[1]);
 
-}
-void terminate(){
-    printf("Process %d: my PID is %d: I completed my task and I am exiting.\n"
-           "Highest prime number I found is %llu.\n",process, getpid(), prime);
-    boolean = 2;
+    //init the signals for suspending, resuming, and terminating
+    signal(continue_signal, child_handler);
+    signal(suspend_signal, child_handler);
+    signal(terminate_signal, child_handler);
+
+    //generate random number
+    rand_num = rand_num_gen();
+
+    //run the starting function
+    start();
+
+    //stay until terminated
+    while(1){
+        //if terminated(flag=-1) return 0
+        if(flag == -1){
+            return 0;
+        }
+    }
 }
--- dekoskitarnowski2/rrScheduler.c
+++ dekoskitarnowski2/rrScheduler.c
@@ -17,88 +17,127 @@ int* id_burst;
 struct sigaction sa;
 struct itimerval timer;
 
+int handler_index = 0;
+int bt_time;
+int clock_timer = 0;
+int length = 0;
+int completed_count = 0;
+
+const int suspend_signal = 10;
+const int continue_signal = 11;
+const int terminate_signal = 12;
+
+/*
+Function Name: RoundRobin
+Input to the method: process id as an integer
+Output(Return value): the pid of the child
+Brief description of the task:
+Fork a child and return the pid
+*/
 int RoundRobin(int id){
-//    printf("Start Round Robin\n");
     int pid;
     char id_str[10];
     if(id >= 0 && id <= 10){
+        //if the fork failed print error
         if ((pid = fork()) < 0) {
             printf("Fork Failed\n");
             fflush(stdout);
         } else if (pid == 0) { //child
             sprintf(id_str, "%d", id);
-            char *args[] = {"./prime", id_str, NULL};
+            char* args[] = {"./prime", id_str, NULL};
+            //run the prime function as the child
             execvp("./prime", args);
         }
     }
+    //return the pid of the child`
     if(pid > 0){
         return pid;
     }
 }
 
-int handler_index = 0;
-int bt_time;
-int clock_timer = 0;
-int length = 0;
-
+/*
+Function Name: timer_handler
+Input to the method: signal as signum
+Output(Return value): None
+Brief description of the task:
+The handler for sending signals to the child processes
+*/
 void timer_handler(int signum){
-    printf("Start Handler: %d\n", signum);
-    if((id_burst[handler_index+1] != 0) && (id_burst[handler_index+3] == SIGCONT)){
-        printf("Suspending: %d\n\n", id_burst[handler_index]);
-        fflush(stdout);
-        kill(id_burst[handler_index+2], SIGSTOP);
-        id_burst[handler_index+3] = SIGSTOP;
-        id_burst[handler_index+1] -= bt_time;
-//        clock_timer+=bt_time;
+    printf("Scheduler: Time Now: %d seconds\n", clock_timer);//print current processing time
+    fflush(stdout);
+    //if there is time left for the process and it is running then suspend it
+    if((id_burst[handler_index+1] != 0) && (id_burst[handler_index+3] == continue_signal)){
+        id_burst[handler_index+3] = suspend_signal;//set the current signal to SIGSTOP
+        id_burst[handler_index+1] -= bt_time;//remove time quantum from the burst time
+
+        //if there is no more time left for the process and it is suspended then terminate it
+        if((id_burst[handler_index+1] == 0) && (id_burst[handler_index+3] == suspend_signal)){
+            id_burst[handler_index+1] = -1;//set the burst time to -1
+            id_burst[handler_index+3] = terminate_signal;//set the current signal to SIGTERM
+            kill(id_burst[handler_index+2], terminate_signal);//signal the child to terminate
+            id_burst[handler_index+2] = -1;//set the child pid to -1
+            completed_count++;//increment completed_count
+        }
+        else{
+            kill(id_burst[handler_index+2], suspend_signal);//signal the child to suspend
+        }
         handler_index+=4;
     }
-    else if(id_burst[handler_index+1] == 0){
-        fflush(stdout);
-        kill(id_burst[handler_index+2], SIGTERM);
-        id_burst[handler_index+3] = SIGTERM;
+    else if(id_burst[handler_index+3] == terminate_signal){
+        handler_index+=4;
     }
 
-    printf("Scheduler: Time Now: %d seconds\n", clock_timer);
-    fflush(stdout);
-    if(((length*2)) <= handler_index){
+    //when the index is more that the length of id_burst reset it to 0
+    if((length*2) <= handler_index){
         handler_index = 0;
     }
 
-    if(id_burst[handler_index+3] == SIGSTOP){
-        fflush(stdout);
-        kill(id_burst[handler_index+2], SIGCONT);
-        id_burst[handler_index+3] = SIGCONT;
+    //if the process is suspended, resume it
+    if((id_burst[handler_index+3] == suspend_signal) && (id_burst[handler_index+1] > 0)){
+        id_burst[handler_index+3] = continue_signal;//set the current signal to SIGCONT
+        kill(id_burst[handler_index+2], continue_signal);//signal the child to resume
     }
-    else if(id_burst[handler_index+3] == 0){
-        id_burst[handler_index+2] = RoundRobin(id_burst[handler_index]);
+    //if the process has not been forked yet(No value in pid(handler_index+2) or current signal(handler_index+3))
+    if((id_burst[handler_index+3] == 0) || (id_burst[handler_index+2] == 0)){
+        id_burst[handler_index+2] = RoundRobin(id_burst[handler_index]);//set the pid
         printf("Scheduling to Process %d (Pid %d) for the time slice of %d seconds.\n\n"
-                ,id_burst[handler_index], id_burst[handler_index+2], bt_time);
+               ,id_burst[handler_index], id_burst[handler_index+2], bt_time);//print the scheduling of the process
         fflush(stdout);
-        kill(id_burst[handler_index+2], SIGCONT);
-        id_burst[handler_index+3] = SIGCONT;
+        id_burst[handler_index+3] = continue_signal;//set the current signal to SIGCONT
     }
 
-    clock_timer+=bt_time;
-//    handler_index+=4;
+    clock_timer+=bt_time;//add time quantum to the clock
 }
 
+/*
+Function Name: main
+Input to the method: Number of arguments(argc) and arguments(argv)
+Output(Return value): 0
+Brief description of the task:
+Take the file and time quantum arguments to use in the handler
+Run a timer that will call the handler every time quantum
+*/
 int main(int argc, char *argv[]){
-    printf("Start main\n");
-
+//    printf("Start main\n");
+    //change the time quantum to an int
     bt_time = atoi(argv[2]);
 
+    //set up file pointers and line buffers
     FILE *infile = NULL;
     FILE *infile2 = NULL;
     char line_buff[100];
     char line_buff2[100];
+
+    //when then arguments are more or less than 3, print error
     if(argc > 3 || argc < 3){
-        perror("The command takes 2 arguments of a file and number\n\n");
+        printf("The command takes 2 arguments of a file and number\n");
         fflush(stdout);
+        exit(1);
     }
     else {
+        //use the file pointer to get the length for id_burst array
         infile2 = fopen(argv[1], "r");
         fgets(line_buff2, 100, infile2);
-
         while (fgets(line_buff2, 100, infile2) != NULL) {
             char *tokens2 = strtok(line_buff2, " ");
             while (tokens2 != NULL) {
@@ -106,6 +145,8 @@ int main(int argc, char *argv[]){
                 tokens2 = strtok(NULL, " ");
             }
         }
+
+        //use file pointer to populate id_burst array with a length of double the input
         id_burst = calloc(length*2, sizeof(int));
         int i = 0;
         int j = 0;
@@ -114,19 +155,16 @@ int main(int argc, char *argv[]){
         while(fgets(line_buff, 100, infile) != NULL){
             char *tokens = strtok(line_buff, " ");
             while (tokens != NULL){
-//                printf("%s\n", tokens);
-                id_burst[i+j] = atoi(tokens);
+                id_burst[j+i] = atoi(tokens);
                 j++;
                 tokens = strtok(NULL, " ");
             }
             i+=2;
         }
         fclose(infile);
+        fclose(infile2);
     }
 
-//    for(int i = 0; i <= 24; i++){
-//        printf("id_burst[%d]: %d\n", i, id_burst[i]);
-//    }
 /* Install timer_handler as the signal handler for SIGALRM. */
     memset(&sa, 0, sizeof (sa));
     sa.sa_handler = &timer_handler;
@@ -142,7 +180,11 @@ int main(int argc, char *argv[]){
 /* Start a real timer. It counts down whenever this process is
 executing. */
     setitimer(ITIMER_REAL, &timer, NULL);
-    while(1) { }
 
-
+    while(1) {
+        if(completed_count == (length/2)) {//when all processed are completed exit
+            exit(0);
+            return 0;
+        }
+    }
 }
